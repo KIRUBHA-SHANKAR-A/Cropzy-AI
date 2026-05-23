151495567d5174ad31e031d9555cd35ced5316d0
Kirubha Shankar A <727723eucs106@skcet.ac.in>
2026-05-23 13:12:56 +0530

    Update app.py

--- ml_service/crop_recommendation/app.py
+++ ml_service/crop_recommendation/app.py
@@ -96,4 +96,5 @@ def health():
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    # app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
